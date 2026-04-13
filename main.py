b357b494db567a737fc353b44046d66f08dd2364
Sobirjon Eshniyazov <esobirjon2197@gmail.com>
2026-04-13 15:53:11 +0500

    Add classes for Bank, Film, Oyin, Universitet, and Restoran

---
+++ main.py
@@ -0,0 +1,69 @@
+
+# 6-m
+class Bank:
+    nom = "Agrobank"
+    valyuta = "UZS"
+    foiz = 20
+
+t1 = Bank()
+print(f"t1 malumotlar: ")
+print(f"obyekyning nom: {t1.nom}")
+print(f"obyekyning valyuta: {t1.valyuta}")
+print(f"obyekyning foiz: {t1.foiz}")
+
+
+
+# 7-m
+class Film:
+    nom = "Avatar"
+    janr = "Fantastika"
+    yil = 2022
+
+t1 = Film()
+print(f"t1 malumotlar: ")
+print(f"obyekyning nom: {t1.nom}")
+print(f"obyekyning janr: {t1.janr}")
+print(f"obyekyning yil: {t1.yil}")
+
+
+
+# 8-m
+class Oyin:
+    nom = "Mafia"
+    platforma = "PC"
+    reyting = 5
+
+t1 = Oyin()
+print(f"t1 malumotlar: ")
+print(f"obyekyning nom: {t1.nom}")
+print(f"obyekyning platforma: {t1.platforma}")
+print(f"obyekyning reyting: {t1.reyting}")
+
+
+# 9-m
+class Universitet:
+    nom = "TATU"
+    davlat = "O‘zbekiston"
+    talabalar_soni = 10000.
+
+t1 = Universitet()
+print(f"t1 malumotlar: ")
+print(f"obyekyning nom: {t1.nom}")
+print(f"obyekyning davlat: {t1.davlat}")
+print(f"obyekyning talabalar_soni: {t1.talabalar_soni}")
+
+
+# 10-m
+
+class Restoran:
+    nom = "Agrobank"
+    manzil = "Toshkent"
+    reyting = 4
+
+t1 = Restoran()
+print(f"t1 malumotlar: ")
+print(f"obyekyning nom: {t1.nom}")
+print(f"obyekyning manzil: {t1.manzil}")
+print(f"obyekyning reyting: {t1.reyting}")
+
+
